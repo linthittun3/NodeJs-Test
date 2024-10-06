a80aedd02c649f7d08265f4516d0bd1095c13c0d
linthittun33 <linthittun77@gmail.com>
2024-10-07 00:54:02 +0700

    Test-1

---
+++ server.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const fs = require('fs');
+const server = http.createServer((req, res)=>{
+    res.setHeader('Content-Type','text/html');
+    fs.readFile('./views/index.html',(err, data)=>{
+        if(err){
+            console.log(err)
+            res.end();
+        }else{
+            res.write(data);
+            res.end();
+        }
+    });
+})
+
+server.listen(3000,'localhost', ()=>{
+    console.log('server listening on port 3000');
+})
